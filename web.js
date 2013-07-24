cac842c8adf49ea0333580ba62a0bc7e969f2e1c
Koji Intlekofer <kintlekofer@gmail.com>
2013-07-24 01:46:19 +0000

    updated web.js with buffer

--- web.js
+++ web.js
@@ -1,10 +1,17 @@
 var express = require('express');
 var app = express();
 app.use(express.logger());
+var content = fs.readFileSync("index.html");
+
+var buffer = new Buffer(content, 'utf-8'); 
+
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(buffer);
 });
+
+
+
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
